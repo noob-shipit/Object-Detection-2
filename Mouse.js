5cc0ec5d5dd20bc6b0c589902b99d931ad1c99ac
noob-shipit <84678441+noob-shipit@users.noreply.github.com>
2023-01-28 13:20:31 -0500

    Add files via upload

---
+++ Mouse.js
@@ -0,0 +1,30 @@
+Status = "";
+Mouse_image = "";
+
+function preload(){
+    Mouse_image = loadImage("Mouse.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect( Mouse_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(Mouse_image,0,0,640,350);
+}
